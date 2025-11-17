4785ad63ce1374c9a2068947f089fc449d2f3aa8
RubixDev <groh@rubixdev.de>
2025-11-17 21:38:26 +0100

    chore: link to fabric api bug report

--- versions/1.21.1-fabric/src/main/java/de/rubixdev/yarrp/mixin/fabric/RecipeOutputMixin.java
+++ versions/1.21.1-fabric/src/main/java/de/rubixdev/yarrp/mixin/fabric/RecipeOutputMixin.java
@@ -9,7 +9,7 @@ import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import net.fabricmc.fabric.api.datagen.v1.recipe.FabricRecipeExporter;
 //#endif
 
-// I thought I liked you Fabric API :'(
+// see https://github.com/FabricMC/fabric/issues/5007 for context
 @Mixin(RecipeOutput.class)
 //#if MC == 12101 || MC >= 12105
 @Restriction(require = @Condition(value = "fabric-api", versionPredicates = { ">=0.116.4 <0.118.0", ">=0.127.0" }))
